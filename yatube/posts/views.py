a2b85e4aa31ad1549a2db1f3fc6b00a04470b99c
Рамиль Абзалов <ramadjamal@yandex.ru>
2022-06-04 19:01:33 +0300

    3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse(f'Главная страница')
+    template = 'posts/index.html'
+    return render(request, template) 
 
 def group_posts(request, slug):
     return HttpResponse(f'Посты {slug}')
