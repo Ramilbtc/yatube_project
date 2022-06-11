73cccfc608b6ac85da78ff8e7a78c65fe09981c6
Рамиль Абзалов <ramadjamal@yandex.ru>
2022-06-11 23:34:31 +0300

    1122

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,19 +1,18 @@
-from distutils.text_file import TextFile
-from tokenize import group
-from turtle import title
-from django.db import models
 from django.contrib.auth import get_user_model
-from email.headerregistry import Group
+from django.db import models
+
 
 User = get_user_model()
+
 
 class Group(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
     description = models.TextField()
-  
+
     def __str__(self):
         return self.title
+
 
 class Post(models.Model):
     text = models.TextField()
@@ -30,6 +29,3 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
         )
-
-
-
